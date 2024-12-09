108781195d7625dd85d0ee2de1fb651d753831c1
admin_dho <davidhorph@hotmail.com>
2024-12-09 08:57:48 -0600

    Moved the modules inside the (new and improved) print function because for now, I don't see using termcolor, colorama or sys anywhere else in main.py. Also, elaborated on print.__doc__.

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from sys import stdout
-from colorama import init;
-from termcolor import colored;
-
-init();
 #Auxillary function that adds border feature.
 def border(borderType="*", text='INSERT TXT HERE'):
    """This border function adds a border feature to the new and improved print function (below)."""
@@ -15,7 +10,14 @@ def print(*objects, end='\n', sep=' ', color=False, borderType=None, caption="Ou
    The New and improved print() function which comes with new and improved features!!!
    There MAY be more to come later...
    If I can get around to it.
+   ANYHOO... with this new and improved version, you can add color (I think it's red, green and yellow and maybe a few more) by doing color='color name' in the parameters. In addition, you can add border with the new and improved print with border = '*' and then it will add a '***' border. Don't like '*' border? Add your own!!!
    """
+   from sys import stdout
+   from colorama import init;
+   from termcolor import colored;
+
+   init();
+
    string= border(borderType=borderType, text=f'{caption}{sep.join(objects[0].split())}') if borderType else f'{caption}{sep.join(objects[0].split())}';
 
    if not color:
@@ -23,8 +25,10 @@ def print(*objects, end='\n', sep=' ', color=False, borderType=None, caption="Ou
    else:
       stdout.write(colored(string, color=color, attrs=["bold", "blink"]) + end);
 
+#TESTS.
 print("THIS SHOULD BE RED...", sep=' - ', end='\n\t===== NEW LINE HERE. =====\n', color='red', caption='changed the caption:');
 print("This should be GREEN and have a border of 0", borderType="0", sep=".", color='green');
+print("THIS IS HOW YOU DO A BORING OLD PRINT (SEE BELOW)", caption='');
 print(print.__doc__, caption='');
 
 
